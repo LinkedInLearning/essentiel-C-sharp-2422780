fcc391fb931be489a26b40753a8df58d93389f12
Rudi Bruchez <rudi@babaluga.com>
2024-01-29 13:59:28 +0100

    C# 12

--- Chapitre_04/04_04_Program.cs
+++ Chapitre_04/04_04_Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace ApplicationConsole
 {
@@ -19,12 +18,15 @@ namespace ApplicationConsole
                 Taille = 3
             };
 
-            string nom = "Noémie et \"Aurélie\"";
-            string chemin = $@"c:\Windows\system {nom} ";
-            //string remarque = String.Format("une chaîne {0} et {1}", "jolie", "sympa");
+            string nom = $$"""
+                Noémie et "Aurélie" 
+                    {{araignee.Couleur}}
+                """;
+            string chemin = $@"c:\Windows\System {nom}";
+            string remarque = String.Format("une chaîne {0} et {1}", "jolie", "sympa");
 
             Console.WriteLine("une chaîne {0} et {1}", "jolie", "sympa");
-            //Console.WriteLine("notre araignée est " + araignee.Couleur);
+            Console.WriteLine("notre araignée est " + araignee.Couleur);
             Console.ReadKey(); // on attend une touche
         }
     }
